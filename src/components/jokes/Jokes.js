cae672de3524ec4ce63dd9a00c29f477e9bfdaa4
Jordan J <jordanr2m@gmail.com>
2024-05-20 12:59:07 -0700

    Finished adding logic to the project.

--- src/components/jokes/Jokes.js
+++ src/components/jokes/Jokes.js
@@ -1,18 +1,55 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
 import "./Jokes.css";
 // Treat this as a default eport: no need to use {}. We can name this import anything
 import smileyFace from "../../assets/smiley.png"
 
 const Jokes = () => {
+  // Variable to store API url
+  const url = "https://api.chucknorris.io/jokes/random";
+
+  // State to store Loading state. When the app opens, the data will need to load first, so we set this to true initially
+  const [isLoading, setIsLoading] = useState(true);
+  // State to store Joke. Set it as an empty object initially
+  const [joke, setJoke] = useState({});
+
+  // Function to fetch the joke
+  async function getJoke() {
+    // set isLoading to true here just to be sure it's true
+    setIsLoading(true);
+
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    setJoke(data);
+
+    // set isLoading to false after operation is finished
+    setIsLoading(false);
+  }
+
+  // useEffect to fetch & display a joke on page load. Set a 2 second delay to make sure everything has loaded before fetching joke
+  useEffect(() => {
+    setTimeout(() => {
+      getJoke();
+      console.log("delay of 2 seconds")
+    }, 2000)
+  }, []); // Add [] to show no dependencies
+  // NOTE! The useEffect hook will fire twice initially (shows 1 joke, then quickly shows another). This is how development mode is set up when using React.StrictMode. In build mode, useEffect will fire only once. See this article for more info: https://stackoverflow.com/questions/60618844/react-hooks-useeffect-is-called-twice-even-if-an-empty-array-is-used-as-an-ar 
+  // When I remove StrictMode tags from index.js, the problem goes away
+
   return (
     <section className='jokes-sec --center-all'>
       <div className='joke --bg-light --card'>
         <h2>Random Joke Generator</h2>
         <img className='smiley' src={smileyFace} alt='Smiley face'/>
         <hr />
-        <p className='--my2'>Lorem sdfijsd pojerm</p>
+        {/* Check if the state is loading or not */}
+        {isLoading ? (
+          <h3>Loading...</h3>
+        ) : (
+          <p className='--my2'>{joke.value}</p>
+        )}
         <hr />
-        <button className='--btn --btn-primary --btn-block'>Generate New Joke</button>
+        <button className='--btn --btn-primary --btn-block' onClick={getJoke}>Generate New Joke</button>
       </div>
     </section>
   )
